3ceeb0e64ae42f084117a38e87f2f87060f89a95
nileshchordia <nileshchordia93@gmail.com>
2018-07-02 11:04:33 +0530

    Basic Coding HackerRank.

---
+++ src/com/cryptohash/Loop.java
@@ -0,0 +1,15 @@
+package com.cryptohash;
+
+import java.util.Scanner;
+
+public class Loop {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(N+"x"+(i+1)+"="+N*(i+1));
+        }
+        scanner.close();
+    }
+}

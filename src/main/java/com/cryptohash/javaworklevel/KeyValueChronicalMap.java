1f2848cdb00b8703c6c7f982c312979acf26e23c
nileshchordia <nileshchordia93@gmail.com>
2019-07-08 12:43:45 +0530

    chronicle map

--- src/main/java/com/cryptohash/javaworklevel/KeyValueChronicalMap.java
+++ src/main/java/com/cryptohash/javaworklevel/KeyValueChronicalMap.java
@@ -1,7 +1,17 @@
 package com.cryptohash.javaworklevel;
 
+import net.openhft.chronicle.map.ChronicleMap;
+
+
+/**
+ * not done
+ */
+
 public class KeyValueChronicalMap {
     public static void main(String[] args) {
-        
+        ChronicleMap<Long,String> inMememoryMap = ChronicleMap.of(Long.class,String.class)
+                .name("country-map").entries(50).averageValue("america").create();
+
+
     }
 }

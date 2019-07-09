ce76336054fd857b1dc5aa0c3b5414e00b175179
nilesh.chordia <nileshchordia93@gmail.com>
2019-07-09 05:55:08 +0530

    Java Date Validation String using JAvA 8 and Commons-Validator Library

--- src/main/java/com/cryptohash/BooleanVariable.java
+++ src/main/java/com/cryptohash/BooleanVariable.java
@@ -1,10 +1,9 @@
 package com.cryptohash;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 public class BooleanVariable {
     public static void main(String[] args) {
-        boolean b=true;
+        boolean b = true;
         System.out.println(b);
     }
 }
--- src/main/java/com/cryptohash/javaworklevel/DateCheckJava8.java
+++ src/main/java/com/cryptohash/javaworklevel/DateCheckJava8.java
@@ -1,8 +1,12 @@
 package com.cryptohash.javaworklevel;
 
+import org.apache.commons.validator.GenericValidator;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
+import java.time.format.ResolverStyle;
+import java.util.Locale;
 
 /**
  * Java 8 DateValid
@@ -20,13 +24,34 @@ public class DateCheckJava8 {
             try {
                 LocalDate.parse(dateStr, dateTimeFormatter);
                 return true;
-            }catch (Exception e){
+            } catch (Exception e) {
                 return false;
             }
         };
 
 
         System.out.println(dateValidator.isValid("2019-07-10"));
+
+        DateValidator validator = dateStr -> {
+            java.time.format.DateTimeFormatter dateTimeFormatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.US)
+                    .withResolverStyle(ResolverStyle.LENIENT);
+
+            try {
+                dateTimeFormatter.parse(dateStr);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+
+
+        };
+
+        System.out.println(validator.isValid("2019-09-20"));
+        System.out.println(GenericValidator.isDate("2019-09-12", "yyyy-MM-dd", true));
+        System.out.println(GenericValidator.isBlankOrNull(""));
+        System.out.println(GenericValidator.isBlankOrNull(null));
+        System.out.println(GenericValidator.isBlankOrNull("nilesh"));
+
 
     }
 

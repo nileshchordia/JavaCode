f4d4eade13aa7b18d4b652bef6040eca322c35ca
nileshchordia <nileshchordia93@gmail.com>
2019-07-10 18:12:12 +0530

    Java 9 takeWhile() streams is similar to Java 7 break;

---
+++ src/main/java/com/cryptohash/javaworklevel/OnlyJava9BreakTakeWhile.java
@@ -0,0 +1,36 @@
+package com.cryptohash.javaworklevel;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * java 7's break is similar to java 9's takeWhile
+ */
+
+public class OnlyJava9BreakTakeWhile {
+    public static void main(String[] args) {
+
+        /**
+         * java 9's takeWhile  (jabki lelo)
+         */
+
+        Stream.of("cat", "monkey", "dog", "horse", "donkey")
+                .takeWhile(s -> s.length() % 2 != 0).forEach(System.out::println);
+
+
+        /**
+         * java 7 break
+         */
+
+        List<String> cat = List.of("cat", "monkey", "dog", "horse", "donkey");
+
+        for (String s : cat) {
+            if (s.length() % 2 == 0) {
+                break;
+            }
+            System.out.println(s);
+        }
+
+
+    }
+}

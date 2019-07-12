27b797282b958e1c927427b4ed1ec3c025a2f2d5
nileshchordia <nileshchordia93@gmail.com>
2019-07-12 23:02:53 +0530

    JAVA STRING REVERSE MULTIPLE WAY

--- src/main/java/com/cryptohash/javaworklevel/ReverseString.java
+++ src/main/java/com/cryptohash/javaworklevel/ReverseString.java
@@ -1,9 +1,12 @@
 package com.cryptohash.javaworklevel;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class ReverseString {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
@@ -35,6 +38,53 @@ public class ReverseString {
         stringBuilder.reverse();
         System.out.println(stringBuilder);
 
+        /**
+         * Common lang 3
+         */
+
+        System.out.println("APACHE COMMON LANG");
+
+        String reverse = StringUtils.reverse(input);
+        System.out.println(reverse);
+
+        /**
+         * Alternate Traditional Approach
+         */
+
+
+        System.out.println("TRADITIONAL APPROACH");
+        String reverseString = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverseString = reverseString + input.charAt(i);
+        }
+
+        System.out.println(reverseString);
+
+
+        /**
+         * reverse order of words in a sentence in a Tradtional Approach
+         */
+
+        String reverseSentence = reverseOrderOfWords("nilesh jain");
+        System.out.println(reverseSentence);
+
+        /**
+         *reverse order of words in a sentence using String Builder
+         */
+//        reverseOrderOfWords()
 
     }
+
+    public static String reverseOrderOfWords(String sentence) {
+        String regex = " ";
+        String[] split = sentence.split(regex);
+        String reverse = "";
+        for (int i = split.length - 1; i >= 0; i--) {
+            reverse = reverse + split[i] + " ";
+        }
+        return reverse.trim();
+
+    }
+
+
 }

9ac7aeae12beada5518f4cac5cad2e4fcabe2d40
nileshchordia <nileshchordia93@gmail.com>
2019-07-15 22:24:34 +0530

    MultiLine String

---
+++ src/main/java/com/cryptohash/javaworklevel/MultiLineString.java
@@ -0,0 +1,62 @@
+package com.cryptohash.javaworklevel;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Properties;
+
+public class MultiLineString {
+    public static void main(String[] args) {
+        String newLine = System.getProperty("line.separator");
+        /**
+         * Java 8 Approach
+         */
+
+        String join = String.join(newLine, "nilesh", "jain", "love", "coding");
+        System.out.println(join);
+        System.out.println(newLine);
+
+        /**
+         * string cocat
+         */
+        String concat = "".concat("nilesh").concat(newLine).concat("jain").concat(newLine).concat("love").concat(newLine).concat("coding").concat(newLine);
+        System.out.println(concat);
+        System.out.println(newLine);
+
+        /**
+         * Java Compiler convert concat() and + into same way
+         */
+
+        String s = "" + newLine + "nilesh" + newLine + "love" + newLine + "coding";
+        System.out.println(s);
+        System.out.println(newLine);
+
+        /**
+         * String Buffer same as String Builder
+         */
+
+        StringBuffer stringBuffer = new StringBuffer().append(newLine).append("nilesh").append(newLine).append("love")
+                .append(newLine).append("coding");
+        System.out.println(stringBuffer);
+
+        /**
+         * String Writer
+         */
+
+        StringWriter stringWriter = new StringWriter().append(newLine).append("nilesh").append(newLine).append("love")
+                .append(newLine).append("coding");
+
+        System.out.println(stringWriter.toString());
+
+        /**
+         * String Writer and Print Writer
+         */
+
+        StringWriter stringWriter1 = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter1);
+        printWriter.println("nilesh");
+        printWriter.println("love");
+        printWriter.println("coding");
+        System.out.println(stringWriter1.toString());
+
+    }
+}
